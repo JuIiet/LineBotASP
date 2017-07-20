024cc0c6d4bbab6fba41fb8f2f809229e6474694
JuIiet <Juliet_065@hotmail.com>
2017-07-20 14:21:17 +0700

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace safeprojectname
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //System.Diagnostics.Debugger.Break();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            // Code that runs when an unhandled error occurs
+            if (HttpContext.Current != null)
+            {
+                var url = HttpContext.Current.Request.Url;
+                var page = HttpContext.Current.Handler as System.Web.UI.Page;
+            }
+        }
+    }
+}
